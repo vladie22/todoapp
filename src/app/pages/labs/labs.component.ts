3de540aa1443f331e99430b1ffb1933e2f49b810
Vladimir Espinoza <vladi981022@gmail.com>
2024-01-25 14:05:14 -0700

    add style in global as local in components

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
+  welcome = "Welcome to my first project with Angular";
+  tasks = [
+    "take out the garbage",
+    "wash dishes",
+    "wash the car",
+    "goinf to market",
+  ]
 
 }
